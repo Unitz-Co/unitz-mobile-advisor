edb1263291aa5d8ce0fee25d6772000db5273700
Hung Tran <hungtran0203@gmail.com>
2021-06-19 15:03:44 +0700

    #ci

--- .github/src/build.sh
+++ .github/src/build.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-kojo dir src/workflows --save build/workflows --import src/steps
+#kojo dir src/workflows --save build/workflows --import src/steps
+kojo dir src/workflows --save workflows --import src/steps
